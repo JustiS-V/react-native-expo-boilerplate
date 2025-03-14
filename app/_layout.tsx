82673aaa7c4503e73757d476236007dc05b81e69
Valentyn Shvedov <bobrshv@gmail.com>
2025-03-14 12:23:27 +0200

    feat: add main layout routing with stack navigator

---
+++ app/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+
+export default function RootLayout() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(auth)/login" />
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+    </>
+  );
+}
